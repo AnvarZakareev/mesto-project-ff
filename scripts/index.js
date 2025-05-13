6c73316423681f6b628b3f79b4e5858fbbffa634
AnvarZakareev <zanvarr@mail.ru>
2025-05-13 22:19:03 +0800

    221813052025

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,42 @@
 
 // @todo: Темплейт карточки
-// 1
-let placesList = document.querySelector('.places__list');
 
-function addCard (card, callback) {
+const placesList = document.querySelector('.places__list');
+
+function createCard (card, callback) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   cardElement.querySelector('.card__image').src = card.link;
   cardElement.querySelector('.card__title').textContent = card.name;
+  // То есть эта функция должна только возвращать один экземпляр карточки,
+  // но не добавлять его в общий список.
   placesList.append(cardElement);
   const cardDelBtn = document.querySelectorAll('.card__delete-button');
+  // В функции createCard функция удаления карточки  должна быть добавлена 
+  // как обработчик на одну кнопку cardDeleteButton, без вызова цикла forEach
   cardDelBtn.forEach(function(elem) {
     elem.addEventListener('click', callback)
     })
+  // Функция createCard должна получать элемент карточки cardElement
+  // из клонированного шаблона карточки и возвращать этот элемент в инструкции return.
   return cardElement;
 };
 
-function delFunc(evt) {
-    const deleteButton = evt.target;
-    const listItem = deleteButton.closest('.card');
-    listItem.remove();
+function deleteCard(evt) {
+    const cardElement = evt.target.closest('.card');
+    cardElement.remove();
 }
 
-initialCards.forEach(function (item) { 
-  addCard(item, delFunc);
+// Добавляться в общий список методом append элемент карточки должен в методе forEach,
+// вызываемом вне функции createCard.
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+   placesList.append(cardElement);
 });
+
+// initialCards.forEach(function (item) { 
+//   addCard(item, deleteCard);
+// });
 
 // @todo: DOM узлы
 
