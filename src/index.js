60acb5fd60e01e8e83eb5c7e55954ba0f47360b3
AnvarZakareev <zanvarr@mail.ru>
2025-05-31 23:42:52 +0800

    2342310525

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-import './styles/index.css';
 
-console.log('Hello, World!')
+// @todo: Темплейт карточки
 
-const numbers = [2, 3, 5];
+const placesList = document.querySelector('.places__list');
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
+// Функция, которая принимает в аргументах данные одной карточки
+// и функцию-колбэк для удаления, а возвращает подготовленный к выводу элемент карточки
+function createCard (card, callback) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener("click", callback);
+  return cardElement;
+};
 
-console.log(doubledNumbers); // 4, 6, 10
+// При клике по иконке удаления выбранная карточка должна удаляться со страницы
+function deleteCard(evt) {
+    const cardElement = evt.target.closest('.card');
+    cardElement.remove();
+}
+
+// Используя полученную функцию, выведите все карточки из массива на страницу в элемент .places__list.
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+   placesList.append(cardElement);
+});
+
+// @todo: DOM узлы
+
+// @todo: Функция создания карточки
+
+// @todo: Функция удаления карточки
+
+// @todo: Вывести карточки на страницу
