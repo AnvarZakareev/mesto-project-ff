6bf5fde509effc3d854badb0f9af805fc5b41ed1
AnvarZakareev <zanvarr@mail.ru>
2025-07-02 23:22:07 +0800

    2321020725

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 export {enableValidation, clearValidation};
 
@@ -115,3 +116,27 @@ function enableValidation (settings) {
   errorElement.textContent = '';
 });
 };
+=======
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// вызов функций enableValidation и clearValidation должен находиться в файле index.js.
+// А все другие функции, включая декларирование функции enableValidation и валидации форм,
+// — в отдельном файле validation.js.
+
+
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
+
+
+// очистка ошибок валидации вызовом clearValidation
+
+clearValidation(profileForm, validationConfig);
+>>>>>>> 98dc585 (2321020725)
